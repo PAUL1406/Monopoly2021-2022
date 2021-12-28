57b4ce42d262001efbdb7bf4cd4ac22635dbb45f
Julien <96548936+jlnlin@users.noreply.github.com>
2021-12-28 13:35:59 +0100

    Update banque.c

--- banque.c
+++ banque.c
@@ -1,20 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void Banque(int *joueur, int *montant, int position)
+int Banque(int *joueur, int *montant, int position)
 {
     if (position == 0)
     {
         montant = montant + 200;
     }
 
-    if ((position == 2) || (position == 17))
+    if ((position == 2) || (position == 17) || (position == 33))
     {
         montant = communaute();
     }
 
-    if ((position == 7) || (position == 22))
+    if ((position == 7) || (position == 22) || (position == 36))
     {
         montant = chance();
     }
+
+    if (position == 4)
+    {
+        montant = montant - /// montant à définir;
+    }
+
+    if (position == 12)
+    {
+        montant = montant - /// montant à définir;
+    }
+
+    if (position == 28)
+    {
+        montant = montant - /// montant à définir;
+    }
+
+    if (position == 38)
+    {
+        montant = montant - /// montant à définir;
+    }
+    
+    return montant;
 }
