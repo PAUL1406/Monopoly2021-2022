ea4bbbab427819bad33a78734b452dc4b87d36db
Julien <96548936+jlnlin@users.noreply.github.com>
2021-12-30 16:47:17 +0100

    Update LancerDesDes.c

--- LancerDesDes.c
+++ LancerDesDes.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int LancerDesDes()
+int main()
 {
     /// déclaration des variables
     int de1, de2, min = 1, max = 6, SommeDesDes;
@@ -34,13 +34,28 @@ int LancerDesDes()
         /// somme des dés
         SommeDesDes = SommeDesDes + de1 + de2;
 
-        if (de1 == de2)
+        if (de1 == de2) /// en cas de doublon, les dés sont relancés
         {
-            SommeDesDes = 0; /// le joueur part en prison au deuxième doublon des dés
+            printf ("Vous avez fait deux doublons : les des sont relances... \n");
 
-            printf ("Vous avez fait deux doublons : vous vous dirigez donc vers la prison... \n");
+            /// affectation d'un nombre aléatoire compris entre 1 et 6
+            de1 = rand()%(max - min + 1) + min;
+            de2 = rand()%(max - min + 1) + min;
+
+            /// affichage des valeurs des 2 dés
+            printf ("Le resultat du premier de est : %d \n", de1);
+            printf ("Le resultat du deuxieme de est : %d \n", de2);
+
+            /// somme des dés
+            SommeDesDes = SommeDesDes + de1 + de2;
+
+            if (de1 == de2)
+            {
+                SommeDesDes = 0; /// le joueur part en prison au troisième doublon des dés
+
+                printf ("Vous avez fait trois doublons : vous vous dirigez donc vers la prison... \n");
+            }
         }
     }
-
     return SommeDesDes;
 }
