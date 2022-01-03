6d3826af5d8cc1c7991fe176ccb2177cc3789f4a
Julien <96548936+jlnlin@users.noreply.github.com>
2022-01-03 21:55:04 +0100

    Update banque.c

--- banque.c
+++ banque.c
@@ -9,36 +9,20 @@ void banque(int *joueur, int *montant, int *position, int *trouNoir)
         montant = montant + 200;
     }
 
-    if ((position == 2) || (position == 17) || (position == 33)) /// Si le joueur tombe sur une case caisse de communauté
+    if ((position == 10) || (position == 18) || (position == 26)) /// Si le joueur tombe sur une case caisse de communauté
     {
         communaute(joueur, montant, position, trouNoir);
     }
 
-    if ((position == 7) || (position == 22) || (position == 36)) /// Si le joueur tombe sur une case chance
+    if ((position == 6) || (position == 14) || (position == 22)) /// Si le joueur tombe sur une case chance
     {
         chance(joueur, montant, position, trouNoir, maison, hotel);
     }
 
     /// Si le joueur tombe sur une case de taxe :
-    if (position == 4)
-    {
-        printf ("Vous passez par un peage. Vous payez 200 euros. \n");
-        montant = montant - 200;
-    }
-
-    if (position == 12)
-    {
-        montant = montant - /// montant à définir;
-    }
-
-    if (position == 28)
-    {
-        montant = montant - /// montant à définir;
-    }
-
-    if (position == 38)
+    if (position == 2)
     {
         printf ("Vous vous faites piller. Vous payez 100 euros. \n");
-        montant = montant - 100;
+        montant = montant - 200;
     }
 }
