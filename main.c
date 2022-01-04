098e9d10abafd8b32a90175ff2522c79ba377833
RomainLEcrack <96549983+RomainLEcrack@users.noreply.github.com>
2022-01-04 12:16:10 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Proprietes Proprietes;
+
+int main()
+{
+    return 0;
+}
+
+struct Proprietes
+{
+        char nomCase[30];
+        char couleur[15];
+        int position;
+        int prix;
+        char proprietaire[30];
+        int maisons;
+        int hotel;
+        bool hypotheque;
+
+};Proprietes
+
+void cases
+{
+    ///CASE 1 -------- DEPART --------
+    strcpy(Proprietes[0].nomCase, "DEPART");
+    strcpy(Proprietes[0].
+}
