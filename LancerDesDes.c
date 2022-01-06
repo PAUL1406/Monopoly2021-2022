35ce56a9b6cc4e9a9e6c725b527dffeb51da826b
Julien <96548936+jlnlin@users.noreply.github.com>
2022-01-06 15:02:54 +0100

    Update LancerDesDes.c

--- LancerDesDes.c
+++ LancerDesDes.c
@@ -4,7 +4,7 @@
 int main()
 {
     /// déclaration des variables
-    int de1, de2, min = 1, max = 6, SommeDesDes;
+    int de1, de2, min = 1, max = 6, SommeDesDes, DoubleDeDes;
 
     srand (time(NULL));
 
@@ -13,49 +13,63 @@ int main()
     de2 = rand()%(max - min + 1) + min;
 
     /// affichage des valeurs des 2 dés
-    printf ("Le resultat du premier de est : %d \n", de1);
-    printf ("Le resultat du deuxieme de est : %d \n", de2);
+    printf ("Le resultat du premier de est : %d. \n", de1);
+    printf ("Le resultat du deuxieme de est : %d. \n", de2);
 
     /// somme des dés
     SommeDesDes = de1 + de2;
 
-    if (de1 == de2) /// en cas de doublon, les dés sont relancés
-    {
-        printf ("Vous avez fait un doublon : les des sont relances... \n");
+    /// affichage de l'avancement
+    printf ("Vous avancez de %d cases. \n", SommeDesDes);
 
+    if (de1 == de2) /// en cas de double, les dés sont relancés
+    {
+        printf ("Vous avez fait un doublon : vous pouvez rejouer. \n");
+
+        // mettre tous les sous-programme
+        
+        // sous programme de blindage
+        
         /// affectation d'un nombre aléatoire compris entre 1 et 6
         de1 = rand()%(max - min + 1) + min;
         de2 = rand()%(max - min + 1) + min;
 
         /// affichage des valeurs des 2 dés
-        printf ("Le resultat du premier de est : %d \n", de1);
-        printf ("Le resultat du deuxieme de est : %d \n", de2);
+        printf ("Le resultat du premier de est : %d. \n", de1);
+        printf ("Le resultat du deuxieme de est : %d. \n", de2);
 
         /// somme des dés
-        SommeDesDes = SommeDesDes + de1 + de2;
+        SommeDesDes = de1 + de2;
 
-        if (de1 == de2) /// en cas de doublon, les dés sont relancés
+        /// affichage de l'avancement
+        printf ("Vous avancez de %d cases. \n", SommeDesDes);
+
+        if (de1 == de2) /// en cas de double, les dés sont relancés
         {
-            printf ("Vous avez fait deux doublons : les des sont relances... \n");
+            printf ("Vous avez fait deux doubles : vous pouvez rejouer. \n");
 
             /// affectation d'un nombre aléatoire compris entre 1 et 6
             de1 = rand()%(max - min + 1) + min;
             de2 = rand()%(max - min + 1) + min;
 
             /// affichage des valeurs des 2 dés
-            printf ("Le resultat du premier de est : %d \n", de1);
-            printf ("Le resultat du deuxieme de est : %d \n", de2);
+            printf ("Le resultat du premier de est : %d. \n", de1);
+            printf ("Le resultat du deuxieme de est : %d. \n", de2);
 
             /// somme des dés
-            SommeDesDes = SommeDesDes + de1 + de2;
+            SommeDesDes = de1 + de2;
+
+            /// affichage de l'avancement
+            printf ("Vous avancez de %d cases. \n", SommeDesDes);
 
             if (de1 == de2)
             {
                 SommeDesDes = 0; /// le joueur part en prison au troisième doublon des dés
 
-                printf ("Vous avez fait trois doublons : vous vous dirigez donc vers la prison... \n");
+                printf ("Vous avez fait trois doubles : vous vous dirigez donc vers la prison... \n");
             }
         }
     }
+
     return SommeDesDes;
 }
